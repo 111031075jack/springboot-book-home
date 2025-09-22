cf9f914844f0e8ea1c752537d2ba5aca7fe5eec3
111031075jack <111031075@live.asia.edu.tw>
2025-09-23 01:44:47 +0800

    update

--- src/main/java/com/example/demo/repository/BookDaoImpl.java
+++ src/main/java/com/example/demo/repository/BookDaoImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -38,20 +39,26 @@ public class BookDaoImpl implements BookDao{
 
 	@Override
 	public Optional<Book> findById(Integer id) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		String sql = "select id, title, price, stock, published from book where id = ?";
+		try {
+			Book book = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Book.class), id);
+			return Optional.ofNullable(book);
+		} catch (Exception e) {
+			return Optional.empty();
+		}
+		
 	}
 
 	@Override
 	public List<Book> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select id, title, price, stock, published from book";
+		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Book.class));
 	}
 
 	@Override
 	public List<Book> findByPage(int start, int size) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select id, title, price, stock, published from book limit ? offset ?";
+		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Book.class), size, start);
 	}
 
 }
---
+++ src/main/java/com/example/demo/service/BookServiceImpl.java
@@ -0,0 +1,67 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.exception.BookNotFoundException;
+import com.example.demo.model.dto.BookDTO;
+import com.example.demo.model.entity.Book;
+import com.example.demo.repository.BookDao;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	@Autowired
+	private BookDao bookDao;
+	
+	@Override
+	public List<BookDTO> findAllBooks() {
+		List<Book> books = bookDao.findAll();
+		// 逐筆 Book 轉 BookDTO
+		List<BookDTO> bookDTOs = books.stream()
+				.map(book -> modelMapper.map(book, BookDTO.class)).toList();
+		return bookDTOs;
+	}
+
+	@Override
+	public BookDTO findBookById(Integer id)throws BookNotFoundException {
+		Optional<Book> optBook = bookDao.findById(id);
+		if(optBook.isEmpty()) {
+			throw new BookNotFoundException("查無書籍, id=" + id);
+		}
+		// 取得 book 物件
+		Book book = optBook.get();
+		// Book 轉 BookDTO
+		BookDTO bookDTO = modelMapper.map(book, BookDTO.class);
+		return bookDTO;
+	}
+
+	@Override
+	public BookDTO addBook(BookDTO bookDTO) {
+		// BookDTO 轉 Book
+		Book book = modelMapper.map(bookDTO, Book.class);
+		// 新增
+		bookDao.save(book);
+		return bookDTO;
+	}
+
+	@Override
+	public BookDTO updateBook(Integer id, BookDTO bookDTO) throws BookNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteBook(Integer id) throws BookNotFoundException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
